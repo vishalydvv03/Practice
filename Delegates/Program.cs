7c315f15c0e9f68efb590d0abd47f6a601330dc5
Vishal Yadav <vishaljyadav576@gmail.com>
2025-03-21 15:44:24 +0530

    Initial Commit

---
+++ Delegates/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    delegate void PrintMsgDelegate(string msg);
+    class Program
+    {
+        public void PrintMsg(string name)
+        {
+            Console.WriteLine("Hello, "+ name);
+        }
+        static void Main(string[] args)
+        {
+            
+            Program p1 = new Program();
+
+            //Normal Method
+
+            //PrintMsgDelegate print = new PrintMsgDelegate(p1.PrintMsg);
+            //print.Invoke("Vishal");
+
+            //Anonymous Method
+
+            //PrintMsgDelegate print = delegate (string name) {
+            //    Console.WriteLine("Enter your name : ");
+            //    name = Console.ReadLine();
+            //    Console.WriteLine($"Hello {name}, Have a good day !!");
+            //};
+
+            //print("Vishal");
+
+            //Lambda Expressions
+
+            PrintMsgDelegate print = (name) => {
+                Console.WriteLine("Enter your name : ");
+                name = Console.ReadLine();
+                Console.WriteLine($"Hello {name}, Have a good day !!");
+            };
+
+            print("Vishal");
+
+
+        }
+    }
+}
