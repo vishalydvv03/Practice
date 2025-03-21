a2446b9dba3d391e8d0cd72ba5c7fdb2f04d7906
Vishal Yadav <vishaljyadav576@gmail.com>
2025-03-21 15:51:36 +0530

    Count Char Problem

---
+++ CountCharacters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char[] ch = s.ToCharArray();
+            int upper = 0;
+            int lower = 0;
+
+
+            for(int i=0; i<s.Length; i++)
+            {
+                if (ch[i]>='A' && ch[i] <= 'Z')
+                {
+                    upper++;
+                }
+                if (ch[i] >= 'a' && ch[i] <= 'z')
+                {
+                    lower++;
+                }
+            }
+            Console.WriteLine($"Upper character count is {upper} and Lower character count is {lower}");
+        }
+    }
+}
