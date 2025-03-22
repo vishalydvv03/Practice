c22213f9376932591545e8c8b4285b7683b600a6
Vishal Yadav <vishaljyadav576@gmail.com>
2025-03-22 18:00:09 +0530

    Added .gitignore file

---
+++ GenericsPractice/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class GenericClass<T>
+{
+    private T data;
+
+    public GenericClass(T value)
+    {
+        data = value;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Value: {data}");
+    }
+}
+
+class GenericMethodEx
+{
+    public void Show<T>(T item)
+    {
+        Console.WriteLine($"Item: {item}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        GenericClass<int> intObj = new GenericClass<int>(10);
+        intObj.Display();
+
+        GenericClass<string> stringObj = new GenericClass<string>("Hello");
+        stringObj.Display();
+
+        GenericMethodEx example = new GenericMethodEx();
+        example.Show(100);
+        example.Show("Generic Method");
+        Console.ReadKey();
+    }
+}
---
+++ MultithreadingEx/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+class MultiThreadExample
+{
+    static void PrintNumbers()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"Thread 1: {i}");
+            Thread.Sleep(500);
+        }
+    }
+
+    static void PrintAlphabets()
+    {
+        for (char c = 'A'; c <= 'E'; c++)
+        {
+            Console.WriteLine($"Thread 2: {c}");
+            Thread.Sleep(500);
+        }
+    }
+
+    static void Main()
+    {
+        Thread thread1 = new Thread(PrintNumbers);
+        Thread thread2 = new Thread(PrintAlphabets);
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine("Main thread completed");
+    }
+}
